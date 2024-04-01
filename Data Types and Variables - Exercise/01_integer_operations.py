eb6299c98df468a356f9d0ae924d8111c0f0af71
darinkirov <darinkirovmid@abv.bg>
2024-04-01 18:03:59 +0300

    Data Types and Variables - Exercise

---
+++ Data Types and Variables - Exercise/01_integer_operations.py
@@ -0,0 +1,10 @@
+from math import ceil, floor
+
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+num_4 = int(input())
+
+result = (num_1 + num_2)//num_3*num_4
+
+print(f"{result:.0f}")
---
+++ Data Types and Variables - Exercise/02_chars_to_string.py
@@ -0,0 +1,7 @@
+def chars_to_string():
+    char1 = input()
+    char2 = input()
+    char3 = input()
+    one_string = char1 + char2 + char3
+    print(one_string)
+chars_to_string()
